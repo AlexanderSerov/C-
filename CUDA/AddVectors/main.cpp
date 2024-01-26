783e3df01107178a933b0ab737e9bab5160551e6
Aleksandr Serov <a.serov@visionlabs.ai>
2024-01-27 00:50:26 +0500

    add prefetch for Unified memory

--- CUDA/AddVectors/main.cpp
+++ CUDA/AddVectors/main.cpp
@@ -1,19 +1,33 @@
+/*
+ * Set up environment for compiling cuda:
+ * 1) Install compatible toolchain VS 2017 or 2019. Install it through separate installer to get full functionality.
+ * 2) You can compile in Command Prompt now using nvcc. For using Powershell x64 follow instructions in Workflow
+ * 		document on Drive.
+ * 3) To be able VS toolset locate you cuda installation you need copy nvidia extension for VS to VS toolset.
+ * 		Details: https://github.com/mitsuba-renderer/mitsuba2/issues/103#issuecomment-618378963
+ * 4) If you use CLion you just need to specify desired, preconfigured toolset.
+ * */
 #include <iostream>
-#include <cmath>
 
-// function to add the elements of two arrays
-void add(const int n, const float *x, float *y)
+
+// Kernel function to add the elements of two arrays
+__global__
+void add(int n, const float *x, float *y)
 {
-	for (int i = 0; i < n; i++)
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+	for (int i = index; i < n; i += stride)
 		y[i] = x[i] + y[i];
 }
 
 int main()
 {
-	int N = 1<<20; // 1M elements
+	int N = 1<<20;
+	float *x, *y;
 
-	auto *x = new float[N];
-	auto *y = new float[N];
+	// Allocate Unified Memory – accessible from CPU or GPU
+	cudaMallocManaged(&x, N*sizeof(float));
+	cudaMallocManaged(&y, N*sizeof(float));
 
 	// initialize x and y arrays on the host
 	for (int i = 0; i < N; i++) {
@@ -21,8 +35,23 @@ int main()
 		y[i] = 2.0f;
 	}
 
-	// Run kernel on 1M elements on the CPU
-	add(N, x, y);
+	// Prefetch the data to the GPU
+	int device = -1;
+	cudaGetDevice(&device);
+	std::cout << "device: " << device << std::endl;
+	cudaMemPrefetchAsync(x, N*sizeof(float), device, nullptr);
+	cudaMemPrefetchAsync(y, N*sizeof(float), device, nullptr);
+
+	// Run kernel on 1M elements on the GPU
+	int blockSize = 256;
+	int gridSize = (N / blockSize) + 1;
+	add<<<gridSize, blockSize>>>(N, x, y);
+
+	// Prefetch the data to the CPU
+	cudaMemPrefetchAsync(y, N*sizeof(float), cudaCpuDeviceId, nullptr);
+
+	// Wait for GPU to finish before accessing on host
+	cudaDeviceSynchronize();
 
 	// Check for errors (all values should be 3.0f)
 	float maxError = 0.0f;
@@ -31,8 +60,8 @@ int main()
 	std::cout << "Max error: " << maxError << std::endl;
 
 	// Free memory
-	delete [] x;
-	delete [] y;
+	cudaFree(x);
+	cudaFree(y);
 
 	return 0;
 }
